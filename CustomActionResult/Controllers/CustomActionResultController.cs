e6630066da15d022ed5a490d18cdb3fba84c8fba
Chathuranga.Sandun <Chathurangasandunkumara@gmail.com>
2018-03-21 04:57:48 +0530

    Add project files.

---
+++ CustomActionResult/Controllers/CustomActionResultController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using CustomActionResult.CustomAction;
+
+namespace CustomActionResult.Controllers
+{
+    [Route("api/[controller]")]
+    public class CustomActionResultController : Controller
+    {
+        [HttpGet]
+        public StringWriterResult Get()
+        {
+            return new StringWriterResult("sandun");
+        }
+    }
+}
---
+++ CustomActionResult/CustomAction/StringWriterResult.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomActionResult.CustomAction
+{
+    public class StringWriterResult : IActionResult
+    {
+        private byte[] _stringAsByteArray;
+        public StringWriterResult(string stringToWrite)
+        {
+            _stringAsByteArray = Encoding.ASCII.GetBytes(stringToWrite);
+        }
+
+        //depcrited ExecuteResult method
+        //dot net core 2 use async method
+        public Task ExecuteResultAsync(ActionContext context)
+        {
+            context.HttpContext.Response.StatusCode = 200;
+            return context.HttpContext.Response.Body.WriteAsync(_stringAsByteArray, 0, _stringAsByteArray.Length);
+        }
+    }
+}
